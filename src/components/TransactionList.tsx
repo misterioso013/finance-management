4d0a4397a29eaffa9f05d3fc38bec06d2cec72c7
Thainara França <thaifranca01@gmail.com>
2024-07-05 17:11:42 -0300

    feat: Adicionar paginação

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -208,6 +208,30 @@ const TransactionList: React.FC<TransactionListProps> = ({
             </TableBody>
           </Table>
         </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <div className="flex-1 text-sm text-muted-foreground">
+            {table.getFilteredSelectedRowModel().rows.length} de{" "}
+            {table.getFilteredRowModel().rows.length} linha(s) selecionada(s).
+          </div>
+          <div className="space-x-2">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
+          </div>
+        </div>
       </CardContent>
     </Card>
   );
